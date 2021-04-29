c28840c583ac0cf9a2a3691926d9aa6e2d6a881c
givenname y <samuel.y.123abc@gmail.com>
2021-04-30 05:17:42 +0800

    Establish frontend skeleton

--- contactlist/resources/js/vue/app.vue
+++ contactlist/resources/js/vue/app.vue
@@ -1,12 +1,37 @@
 <template>
- <div>
-    Hi Vue
+ <div class='contactlistContainer'>
+    <div class="heading">
+        <h2 id="title">contact list</h2>
+        <addContactForm />
+    </div>
+    <listContact />
  </div>
 </template>
 
 <script>
-// import { defineComponent } from '@vue/composition-api'
-export default {
+    import addContactForm from './addContactForm';
+    import listContact from './listContact';
 
-}
+
+    export default {
+        components: { 
+            addContactForm,
+            listContact
+        }
+    }
 </script>
+
+<style scoped>
+.todoListContainer{
+    width: 30em;
+    margin: auto;
+}
+.heading {
+    background: orange;
+    padding: 1em;
+}
+
+#title {
+    text-align: center;
+}
+</style>
---
+++ contactlist/resources/js/vue/listContact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        list view
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
