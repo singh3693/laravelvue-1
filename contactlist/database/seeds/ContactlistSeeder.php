e4351fa535624d9f03fc619c8579d39e75a327a6
givenname y <samuel.y.123abc@gmail.com>
2021-04-29 19:20:38 +0800

    Create seeder file

---
+++ contactlist/database/seeds/ContactlistSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ContactlistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('member')
+    }
+}
