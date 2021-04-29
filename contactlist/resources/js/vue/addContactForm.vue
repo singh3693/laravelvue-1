056a7158805b9b5699549bc2e22c9b21464e844a
givenname y <samuel.y.123abc@gmail.com>
2021-04-30 05:39:38 +0800

    Install fontawesome

--- contactlist/resources/js/vue/addContactForm.vue
+++ contactlist/resources/js/vue/addContactForm.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        form
+    <div class='addContact'>
+        <input type="text" />
+        <font-awesome-icon
+            icon="plus-square"
+        />
     </div>
 </template>
 
@@ -9,3 +12,18 @@ export default {
     
 }
 </script>
+
+<style scoped>
+.addContact {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+input {
+    background: #e3e3e3;
+    border: 0;
+    outline: none;
+    padding: 0.5em;
+    margin-right: 1em;
+}
+</style>
